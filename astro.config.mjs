0cb253e168d781129031ee897d9aedb74c2ecc1f
domindez <109239601+domindez@users.noreply.github.com>
2025-02-08 18:48:22 +0100

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,10 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-output: "server",
-adapter: cloudflare(),
-integrations: [react()],
-vite: {
-resolve: {
-// Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-// Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-// @ts-ignore
-alias: import.meta.env.PROD && {
-"react-dom/server": "react-dom/server.edge",
-},
-},
-},
+  integrations: [react()],
+  output: 'server',
+  adapter: vercel(),
 });
